3e71ded38f8cbfa95ec9a41eed996eae0d043148
Maximilian Algehed <m.algehed@gmail.com>
2016-06-24 00:11:29 +0200

    Some work on making it relational in the ruby style

--- src/ParallelLayout.hs
+++ src/ParallelLayout.hs
@@ -2,59 +2,55 @@
 module ParallelLayout (
     Pll,
     (<|>),
-    (<>),
-    (><)
+    (<*),
+    (*>)
 )where
+import Prelude hiding ((<*), (*>))
 import Data.GraphViz hiding (Arrow)
 import TraceInternal
 import Control.DeepSeq (NFData)
+import Debug.Trace
 
--- The type of a parallel computation
--- this type is a placeholder for a later
--- type which will likely look more like:
--- (Int, Int) -> Chan a -> Chan b -> SomethingToPutOnParallela
--- and use some kind of "forkOn" function rather
--- than just fork
-type Pll a b = IVar a -> IVar b -> Par ()
+-- Funky type for parallel computations
+type Pll a b = [IVar a] -> [IVar b] -> Par ()
 
 -- | Execute a pure action
-arr f i o = fmap f (get i) >>= put o
+arr [] _ _ = return ()
+arr (f:fs) (i:is) (o:os) =
+    do
+        fork $ fmap f (get i) >>= put o
+        arr fs is os
 
 -- | Compose two operations in parallel
-above u l i o =
+above u l is os =
     do
-        intermA <- new
-        intermB <- new
-        fork $ u i intermA
-        fork $ l i intermB
-        a <- get intermA
-        b <- get intermB
-        put o (a, b)
+        let len  = length is `div` 2
+        let len' = length os `div` 2
+        fork $ u (take len is) (take len' os)
+        fork $ l (drop len is) (drop len' os) 
 
 -- | Compose two operations in sequence
-besides x y i o =
+besides (m,n) x y i o =
     do
-        interm <- new
-        fork $ y interm o
-        x i interm
+        -- | The intermidate results
+        interms <- sequence $ replicate m new 
+        -- | Fork the second action with n/m repetitions of the
+        -- | intermidiate results
+        fork $ y (concat (replicate (n `div` m) interms)) o
+        -- | Run the first computation
+        x i interms
+
+-- | Collapse parallel operations
+collapse (m,n) x y i o = undefined
 
 -- | Compose two operations in parallel
--- | in sequence on the same core
-ontop x y i o =
-    do
-        interm <- new
-        x i interm
-        y interm o
-
--- | Compose two operations in parallel
-(<|>) :: (NFData b, NFData c) => Pll a b -> Pll a c -> Pll a (b, c)
+(<|>) :: (NFData b) => Pll a b -> Pll a b -> Pll a b
 (<|>) = above
 
 -- | Compose two operations in sequence
-(<>) :: (NFData b, NFData c) => Pll a b -> Pll b c -> Pll a c
-(<>) = besides
+(<*) x d = \y i o -> besides d x y i o
+(*>) = ($)
 
--- | Compose two operations in parallel
--- | in sequence on the same core
-(><) :: (NFData b, NFData c) => Pll a b -> Pll b c -> Pll a c
-(><) = ontop
+-- | Collapse parallel operations into fewer parallel operations
+(/*) x d = \y i o -> collapse d x y i o
+(*\) = ($)
