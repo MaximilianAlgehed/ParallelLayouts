d471ecc61b4dd42b5c635f77011f38959b1056d3
Maximilian Algehed <m.algehed@gmail.com>
2016-07-06 21:51:15 +0200

    Towards ladner fisher networks

--- src/ParallelLayout.hs
+++ src/ParallelLayout.hs
@@ -1,42 +1,32 @@
 {-# LANGUAGE GADTs, TypeSynonymInstances #-}
 module ParallelLayout (
-
     Pll,
-
     Layout(..),
-
     act,
-
-    (>->),
-
+    (->-),
     (-=-),
-
-    (>/>),
-
+    (-/-),
     (*>*),
-
-    aboveL,
-
-    aboveN,
-
+    (-!-),
+    belowL,
+    belowN,
     pads,
-
+    liftListOp,
     fan,
-
+    riffle,
+    unriffle,
+    interleave,
     example,
-
-    prefix_fan
-
+    prefix_fan,
+    sklansky
 ) where
-import Prelude hiding ((<*), (*>))
-import Data.GraphViz hiding (Arrow)
-import Control.DeepSeq (NFData)
-import Debug.Trace
+import Data.List
 
+-- | A type for layouts of circuits (parallel computations)
 data Layout a b where
     Pure     :: (a -> b) -> Layout a b
     Pad      :: Layout a a
-    Above    :: Pll a b -> Pll a b -> Layout a b
+    Below    :: Pll a b -> Pll a b -> Layout a b
     Besides  :: Pll a b -> Pll b c -> Layout a c
     Overlay  :: Pll a b -> Pll a c -> Layout a (b, c)
     Collapse :: Pll a b -> Pll [b] c -> Layout a c
@@ -45,7 +35,7 @@ paren s = "("++s++")"
 
 instance Show (Layout a b) where
     show (Pure _)        = "*"
-    show (Above l l')    = paren $ (show (fst l)) ++ " above " ++ (show (fst l'))
+    show (Below l l')    = paren $ (show (fst l)) ++ " below " ++ (show (fst l'))
     show (Besides l l')  = paren $ (show (fst l)) ++ " besides " ++ (show (fst l'))
     show (Overlay l l')  = paren $ (show (fst l)) ++ " overlay " ++ (show (fst l'))
     show (Collapse l l') = paren $ (show (fst l)) ++ " collapse " ++ (show (fst l'))
@@ -65,17 +55,17 @@ pad = (Pad, (1,1))
 
 -- Pad many times
 pads :: Int -> Pll a a
-pads n = aboveN n pad
+pads n = belowN n pad
 
 -- | Compose two operations in parallel
-above u@(_, (inu, ou)) l@(_, (inl, ol)) = (Above u l, (inu+inl, ou+ol)) 
+below u@(_, (inu, ou)) l@(_, (inl, ol)) = (Below u l, (inu+inl, ou+ol)) 
 
 -- | Compose several operations in parallel
-aboveL :: [Pll a b] -> Pll a b
-aboveL = foldl1 above
+belowL :: [Pll a b] -> Pll a b
+belowL = foldl1 below
 
--- | Many aboves...
-aboveN n op = aboveL (replicate n op)
+-- | Many belows...
+belowN n op = belowL (replicate n op)
 
 -- | Compose two operations in sequence
 besides x@(_, (ix,ox)) y@(_, (iy, oy)) = (Besides x y, (ix, oy))
@@ -93,17 +83,24 @@ overlay :: Pll a b -> Pll a c -> Pll a (b, c)
 overlay x@(_, (ix, ox)) y@(_, (iy, oy)) = (Overlay x y, (max ix iy, min ox oy))
 
 -- | Compose two operations in parallel
-(-=-) = above
+(-=-) = below
 
 -- | Compose two operations in sequence
-(>->) = besides
+(->-) = besides
 
 -- | Collapse operations in to another operation
-(>/>) = collapse
+(-/-) = collapse
 
 -- | Overlay one operation on another
 (-&-) :: Pll a b -> Pll a c -> Pll a (b, c)
 (-&-) = overlay
+
+-- | Select item n from m
+select :: Int -> Int -> Pll a a
+select n m = pads m -/- (act (!!n))
+
+-- Selection shorthand
+(-!-) = select
 
 -- An example
 -- The structure represented is
@@ -111,29 +108,63 @@ overlay x@(_, (ix, ox)) y@(_, (iy, oy)) = (Overlay x y, (max ix iy, min ox oy))
 -- input -> (+1)-+-(*3)-+-sum -> output
 --               +-(/3)-+
 example :: Pll Double Double
-example = act (+1) >-> aboveL (acts [(+1), (*3), (/3)]) >/> act sum
+example = act (+1) ->- belowL (acts [(+1), (*3), (/3)]) -/- act sum
 
 -- | Connect things in a parallel prefix manner.
 -- | If p and p' both compute parallel prefix of their input, then p>*>p' computes
 -- | parallel prefix of it's input
---
--- The function is very slow as it does not actually perform the operations in parallel,
--- rather it performs the left operation first and then the right one...
 p@(_, (_, o1)) *>* p'@(_, (i2, _)) =
     (p -=- pads (i2-1))
-    >->
+    ->-
     (pads (o1-1) -=- p')
 
 -- | The fan pattern
 fan :: Pll (a,a) a -> Pll a a -> Pll a a
 fan fanout p@(_, (_, op)) =
     (
-        (pad >-> pads (op+1))
+        (pad ->- pads (op+1))
         -&-
         (pad -=- p)
-    ) >-> (act fst -=- fanout)
+    ) ->- (act fst -=- fanout)
+
+-- | Allows list operations to be performed on a layout
+liftListOp :: Int -> ([a] -> [a]) -> Pll a a
+liftListOp n f = pads n -/- act f ->- belowL [act (!!i) | i <- [0..(n-1)]]
+
+-- | Riffle a list
+riffle :: Int -> Pll a a
+riffle n = liftListOp n riffleList
+    where
+        riffleList lst = helper (take ((length lst) `div` 2) lst) (drop ((length lst) `div` 2) lst)
+        helper [] xs = xs
+        helper xs [] = xs
+        helper (x:xs) (y:ys) = x:y:helper xs ys
+
+-- | Unriffle a list
+unriffle :: Int -> Pll a a
+unriffle n = liftListOp n unriffleList
+    where
+        unriffleList xs = [x | (x, i) <- zip xs [0..], even i] ++ [x | (x,i) <- zip xs [0..], odd i]
+
+-- | Interleave an operation
+interleave :: Pll a b -> Pll a b
+interleave p@(_, (i,o)) = unriffle (2*i) ->- (p -=- p) ->- riffle (2*o)
 
 -- | Primitive prefix sum
 prefix_fan :: Int -> ((a,a)->a) -> Pll a a
 prefix_fan 1 _  = pad 
-prefix_fan n op = fan (aboveN (n-1) (act op)) (prefix_fan (n-1) op)
+prefix_fan n op = fan (belowN (n-1) (act op)) (prefix_fan (n-1) op)
+
+-- | sklansky parallel prefix network
+sklansky :: ((a,a) -> a) -> Int -> Pll a a
+sklansky op 1 = pad
+sklansky op 2 = fan (act op) pad
+sklansky op n = sklansky op ls *>* fan (belowN rs (act op)) (sklansky op rs) 
+    where
+        ls = n `div` 2 + 1
+        -- fan pads with 1 on the left, so we need to reduce the size of
+        -- the right hand argument
+        rs = if even n then
+                n `div` 2 - 1
+             else
+                n `div` 2
--- src/RunSequential.hs
+++ src/RunSequential.hs
@@ -5,7 +5,7 @@ import ParallelLayout
 runSequential :: Pll a b -> ([a] -> [b])
 runSequential ((Pure f), _)              (a:_) = [f a] 
 runSequential (Pad, _)                   (a:_) = [a]
-runSequential ((Above l@(_, (i, _)) l'), _)   as = (runSequential l (take i as)) ++ (runSequential l' (drop i as))
+runSequential ((Below l@(_, (i, _)) l'), _)   as = (runSequential l (take i as)) ++ (runSequential l' (drop i as))
 runSequential ((Besides l@(_, (_, o)) l'@(_, (i, _))), _) as = (runSequential l') $ concat $ replicate (i `div` o) $ (runSequential l) as 
 runSequential ((Overlay l@(_, (i, _)) l'@(_, (i', _))), _) as = zip (runSequential l (take i as)) (runSequential l' (take i' as))
 runSequential ((Collapse x@(_, (ixs, oxs)) y@(_, (iys, oys))), _) as = o
@@ -18,3 +18,11 @@ runSequential ((Collapse x@(_, (ixs, oxs)) y@(_, (iys, oys))), _) as = o
 
 -- | Check that prefix meets the specification
 prop_prefix_scanl xs = length xs > 0 ==> tail (scanl (+) 0 xs) == runSequential (prefix_fan (length xs) (uncurry (+))) xs
+
+-- | Check that sklansky meets the spec
+prop_sklansky xs = length xs > 0 ==> tail (scanl (+) 0 xs) == runSequential (sklansky (uncurry (+)) (length xs)) xs
+
+-- | Check the riffle unriffle relationship
+prop_riffle_unriffle :: [Int] -> Property
+prop_riffle_unriffle xs = length xs > 0 && even (length xs) ==> runSequential (riffle (length xs) ->- unriffle (length xs)) xs == xs &&
+                                            runSequential (unriffle (length xs) ->- riffle (length xs)) xs == xs
