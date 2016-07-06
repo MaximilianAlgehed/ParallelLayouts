bd352e20c57a32fef4d462d68e5b2dbbc8ccd31c
Maximilian Algehed <m.algehed@gmail.com>
2016-07-06 23:29:00 +0200

    NFData is a spanner in the works...

--- src/RunPar.hs
+++ src/RunPar.hs
@@ -1,43 +1,38 @@
+{-# LANGUAGE GADTs #-}
+import Control.DeepSeq
+import TraceInternal
+import ParallelLayout
 -- Funky type for parallel computations
-type Pll a b = ([IVar a] -> [IVar b] -> Par (), (Int, Int))
+type PllPar a b = [IVar a] -> [IVar b] -> Par ()
+
+convert :: (NFData a, NFData b) => Pll a b -> PllPar a b
+convert (Pad, _) = \[i] [o] -> get i >>= put o
 
 -- Shorthand for just one action
-act f = (\[i] [o] -> fmap f (get i) >>= put o, (1,1))
+convert (Pure f, _) = \[i] [o] -> fmap f (get i) >>= put o
 
 -- | Compose two operations in parallel
-above (u, (inu, ou)) (l, (inl, ol)) =
-    ((\is os -> do
-        fork $ u (take inu is) (take ou os)
-        fork $ l (drop inu is) (drop ou os)), (inu+inl, ou+ol)) 
-
--- | Compose several operations in parallel
-aboveL :: [Pll a b] -> Pll a b
-aboveL = foldl1 above
+convert (Below u@(_, (inu, ou)) l@(_, (inl, ol)), _) = \is os -> do
+        fork $ (convert u) (take inu is) (take ou os)
+        fork $ (convert l) (drop inu is) (drop ou os)
 
 -- | Compose two operations in sequence
-besides (x, (ix,ox)) (y, (iy, oy)) =
-    (\i o -> do
+convert (Besides x@(_, (ix,ox)) y@(_, (iy, oy)), _) = \i o -> do
         -- | The intermidate results
         interms <- sequence $ replicate ox new 
         -- | Fork the second action with n/m repetitions of the
         -- | intermidiate results
-        fork $ y (concat (replicate (iy `div` ox) interms)) o
+        fork $ (convert y) (concat (replicate (iy `div` ox) interms)) o
         -- | Run the first computation
-        x i interms, (ix, oy))
-
--- | Compose several operations in sequence
-besidesL :: [Pll a a] -> Pll a a
-besidesL = foldl1 besides
+        (convert x) i interms
 
 -- | Collapse parallel operations
-collapse :: (NFData b) => Pll a b -> Pll [b] c -> Pll a c
-collapse (x, (ixs, oxs)) (y, (iys, oys)) =
-    ((\i o -> do
+convert (Collapse x@(_, (ixs, oxs)) y@(_, (iys, oys)),_) = \i o -> do
         ox <- sequence $ replicate oxs new
         iy <- sequence $ replicate iys new
-        fork $ y iy o
-        x i ox
-        fitInto (replicate iys []) ox iy), (ixs, oys))
+        fork $ (convert y) iy o
+        (convert x) i ox
+        fitInto (replicate iys []) ox iy
     where
         fitInto xs [] iy = sequence_ (zipWith put iy xs)
         fitInto xs ox iy =
@@ -45,22 +40,20 @@ collapse (x, (ixs, oxs)) (y, (iys, oys)) =
                fitInto (zipWith (\x y -> y++[x]) ox' xs) (drop (length iy) ox) iy  
 
 -- | Overlay one operation on another
-overlay :: (NFData b, NFData c) => Pll a b -> Pll a c -> Pll a (b, c)
-overlay (x, (ix, ox)) (y, (iy, oy)) =
-    ((\is os -> do
+convert (Overlay x@(_, (ix, ox)) y@(_, (iy, oy)),_) = \is os -> do
                  oxs <- sequence $ replicate ox new
                  oys <- sequence $ replicate oy new
-                 fork $ x (take ix is) oxs
-                 fork $ y (take iy is) oys
+                 fork $ (convert x) (take ix is) oxs
+                 fork $ (convert y) (take iy is) oys
                  intermx <- sequence $ map get oxs
                  intermy <- sequence $ map get oys
-                 sequence_ $ zipWith put os $ zip intermx intermy), (max ix iy, min ox oy))
+                 sequence_ $ zipWith put os $ zip intermx intermy
 
 -- Run a computation in the par monad, and only return the values produced in the end
-runInPar :: (NFData a) => Pll a b -> [a] -> [b]
-runInPar (pll, (ins, outs)) inputs = fst $ runPar $ do
+runInPar :: (NFData a, NFData b) => Pll a b -> [a] -> [b]
+runInPar pll@(_, (ins, outs)) inputs = fst $ runPar $ do
                                         is <- sequence $ replicate ins new
                                         os <- sequence $ replicate outs new
                                         sequence_ (zipWith put is inputs)
-                                        fork $ pll is os
+                                        fork $ (convert pll) is os
                                         sequence $ map get os
